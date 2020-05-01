a19d52f45163b79513fdd5bf088b7faddb15d84b
Tommy Palmer <hi@tommyp.org>
2020-05-01 09:38:27 +0100

    add lazyloading for images

--- components/bin.js
+++ components/bin.js
@@ -1,9 +1,15 @@
 import React from "react"
+import LazyLoad from 'react-lazyload';
 
 const Bin = ({ fileName, councilCode, councilName, collectionDate }) => {
   return (
     <div className="sm:mb-10">
-      <img src={fileName} />
+      <LazyLoad
+        width={585}
+        height={380}
+      >
+        <img src={fileName} />
+      </LazyLoad>
       <div className="p-4 hidden sm:block">
         <h3 className="text-2xl mb-1" data-local-authority-eng={councilCode}>{councilName}</h3>
         <p className="text-xl font-sans font-light">{collectionDate}</p>

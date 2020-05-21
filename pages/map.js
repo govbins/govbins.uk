e22a362c5578a38a07db97becf361dd8387b4f09
Harry Trimble <harryroberttrimble@gmail.com>
2020-05-21 08:59:07 +0100

    Fix typo

--- pages/map.js
+++ pages/map.js
@@ -9,7 +9,7 @@ const Map = () => {
     <div className="lg:w-2/3 lg:ml-40 pl-5 pr-5 lg:p-0 text-2xl font-rubik">
       <h3 className="">Bins catalogued so far</h3>
       <P>
-        <small>Map made with <A href="https://data.gov.uk/dataset/6ff3fc08-26ff-453e-9289-6420269ba10e/local-authority-districts-counties-and-unitary-authorities-december-2017-map-in-united-kingdom">UK Government Data</A></small>
+        <small>Map made with <A href="https://data.gov.uk/dataset/6ff3fc08-26ff-453e-9289-6420269ba10e/local-authority-districts-counties-and-unitary-authorities-december-2017-map-in-united-kingdom">UK government data</A></small>
       </P>
       <img src="/images/map.svg" />
 

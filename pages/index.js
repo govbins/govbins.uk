37a779e139b152be8edb72d92b1d3c95751ac8d0
Tommy Palmer <hi@tommyp.org>
2020-08-02 09:45:57 +0100

    add latest and oldest sorting

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import binsData from "../src/bins";
 import Bin from "../components/bin";
 import React from "react";
 import Footer from "../components/footer";
+import moment from "moment"
 
 const engAuthorityCodes = async () => {
   return await fetchCodes(
@@ -60,78 +61,142 @@ export async function getStaticProps() {
   };
 }
 
-const sortOptions = [
-  {
-    name: "Latest",
-  },
-  {
-    name: "Oldest",
-  },
-  {
-    name: "Name",
-  },
-  {
-    name: "Colour",
-  },
-]
-
-const sortOption = ({ name, active }) => {
-  return (
-    <li className="inline-block">
-      <a href="#" className={`block px-3 py-1 border-transparent border-t-4 ${active ? 'border-gray-800' : 'hover:border-gray-800'}`} >
-        {name}
-      </a>
-    </li>
-  )
-}
-
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
 
+    this.sortOldest = this.sortOldest.bind(this)
+    this.sortLatest = this.sortLatest.bind(this)
+    this.sortName = this.sortName.bind(this)
+
     this.state = {
-      bins: { ...binsData.bins },
+      bins: [...binsData.bins],
+      sortOptions: {
+        latest: {
+          name: "Latest",
+          active: true,
+          func: this.sortLatest,
+        },
+        oldest: {
+          name: "Oldest",
+          active: false,
+          func: this.sortOldest
+        },
+        az: {
+          name: "Name",
+          active: false,
+          func: null,
+        },
+        colour: {
+          name: "Colour",
+          active: false,
+          func: null,
+        }
+      }
     };
   }
 
-  renderBin(key, binData) {
+  updateNav(newActive) {
+    const options = this.state.sortOptions
+    const newOptions = {}
+
+    Object.keys(options).map((key) => {
+      const existing = options[key]
+      newOptions[key] = {
+        name: existing.name,
+        active: key === newActive ? true : false,
+        func: existing.func,
+      }
+    })
+
+    this.setState({ sortOptions: newOptions })
+  }
+
+  sortLatest(e) {
+    e.preventDefault()
+    const bins = this.state.bins
+
+    this.setState({
+      bins: bins.sort((a, b) => {
+        const aDate = moment(a.collectionDate)
+        const bDate = moment(b.collectionDate)
+
+        if (aDate.isBefore(bDate)) return 1
+        if (aDate.isAfter(bDate)) return -1
+        return 0
+      }),
+    })
+
+    this.updateNav("latest")
+  }
+
+  sortOldest(e) {
+    e.preventDefault()
+    const bins = this.state.bins
+
+    console.log('clicked oldest')
+
+    this.setState({
+      bins: bins.sort((a, b) => {
+        const aDate = moment(a.collectionDate)
+        const bDate = moment(b.collectionDate)
+
+        if (aDate.isAfter(bDate)) return 1
+        if (aDate.isBefore(bDate)) return -1
+        return 0
+      }),
+    })
+
+    this.updateNav("oldest")
+  }
+
+  councilName(bin) {
     let councilName;
-    switch (binData.localAuthorityCountry) {
+    const { localAuthorityCountry, localAuthorityCode } = bin
+    switch (localAuthorityCountry) {
       case "eng":
-        councilName = this.props.engAuthorityCodes[binData.localAuthorityCode];
+        councilName = this.props.engAuthorityCodes[localAuthorityCode];
         break;
       case "sct":
-        councilName = this.props.scotAuthorityCodes[binData.localAuthorityCode];
+        councilName = this.props.scotAuthorityCodes[localAuthorityCode];
         break;
       case "wls":
-        councilName = this.props.welshAuthorityCodes[
-          binData.localAuthorityCode
-        ];
+        councilName = this.props.welshAuthorityCodes[localAuthorityCode];
         break;
       case "ni":
-        councilName = this.props.niAuthorityCodes[binData.localAuthorityCode];
+        councilName = this.props.niAuthorityCodes[localAuthorityCode];
         break;
       default:
         councilName = "";
+        break;
     }
 
-    return <Bin key={key} councilName={councilName} {...binData} />;
+    return councilName
   }
 
   render() {
+    const { sortOptions } = this.state
     return (
       <React.Fragment>
         <div className="bg-gray-400">
           <ul>
-            {sortOptions.map((option, index) => {
-              option.active = index == 2
-              return sortOption(option)
+            {Object.keys(sortOptions).map((option, index) => {
+              const sortOption = sortOptions[option]
+              return <li className="inline-block" key={index}>
+                <a href="#" onClick={sortOption.func} className={`block px-3 py-1 border-transparent border-t-4 ${sortOption.active ? 'border-gray-800' : 'hover:border-gray-800'}`} >
+                  {sortOption.name}
+                </a>
+              </li>
             })}
           </ul>
         </div>
         <section className="grid grid-cols-2 lg:grid-cols-4">
-          {binsData.bins.map((bin, index) => {
-            return this.renderBin(index, bin);
+          {this.state.bins.map((bin, i) => {
+            return <Bin
+              key={i}
+              councilName={this.councilName(bin)}
+              {...bin}
+            />;
           })}
         </section>
         <div className="pl-6 text-2xl">

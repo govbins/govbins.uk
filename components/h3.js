cd2933013c383ceb7e6f62f2e9bb2a9aa4797e29
Harry Trimble <harryroberttrimble@gmail.com>
2020-11-14 15:42:57 +0000

    Upate build page

---
+++ components/h3.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const H2 = ({ children }) => {
+  return (
+    <h2 className="text-2xl mb-6 mt-12">{children}</h2>
+  )
+}
+
+export default H2
--- pages/build.js
+++ pages/build.js
@@ -2,6 +2,7 @@ import React from "react";
 import A from "../components/a";
 import P from "../components/p";
 import H2 from "../components/h2";
+import H3 from "../components/h3";
 import Footer from "../components/footer";
 import Contact from "../components/contact";
 
@@ -17,7 +18,20 @@ const About = () => {
       <div className="lg:w-2/3 lg:ml-40 px-5 pt-5 text-2xl font-rubik">
         <H2>How this site is built</H2>
         <P>
-          Each bin is labelled with open data about local councils in{" "}
+          This site is built using {" "}
+          <A href="https://nextjs.org/">
+            Next.js
+          </A> and {" "}
+          <A href="https://tailwindcss.com/">
+            Tailwind CSS
+          </A>.
+        </P>
+        <P>
+          Originally {" "}<A href="https://www.harrytrimble.co.uk/">Harry</A> built the site with Bootstrap and hosted images on Netlify, which was made it slow and expensive. Thankfully, {" "}<A href="http://tommyp.org/">Tommy</A> got involved and changed all that.
+        </P> 
+        <H3>Open government data</H3> 
+        <P>
+          Each bin image is labelled with open government APIs about local councils in{" "}
           <A href="https://www.registers.service.gov.uk/registers/local-authority-eng">
             England
           </A>
@@ -35,12 +49,24 @@ const About = () => {
           </A>
         </P>
         <P>
-            Map made with{" "}
+            The current {" "}<A href='/map'>map</A> is a SVG exported from a {" "}
             <A href="https://data.gov.uk/dataset/6ff3fc08-26ff-453e-9289-6420269ba10e/local-authority-districts-counties-and-unitary-authorities-december-2017-map-in-united-kingdom">
-              UK government data
-            </A>
+              government vector map
+            </A>, which colours we tweaked bit. 
+        </P>
+        <H3>Hosting and privacy</H3> 
+        <P>
+          Hosted on {" "}<A href="https://www.netlify.com/">Netlify</A>, the site {" "}<A href="https://www.netlify.com/products/analytics/">counts page views and visitors</A> using aggregated data, de-anonymised. Meaning the site doesn't use cookies or any other potentially creepy tracking stuff. 
+        </P>
+        <H3>Web performance</H3>
+        <P>
+          The homepage has lazy loading to decrease it's latency. Also to improve performance images are pasted through {" "}<A href="https://imageoptim.com/">ImageOptim</A> and synced them to {" "}<A href="https://aws.amazon.com/s3/">Amazon S3</A>, when deployed via GitHib.
         </P>
 
+        <H3>Open code</H3>
+        <P>
+          Go to {" "}<A href="https://github.com/govbins/govbins.uk">GitHib</A> to look all the code for this site.
+        </P>
         <Footer />
       </div>
     </>

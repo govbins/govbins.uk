9c0ca399f0d49cf5da602bcd29d095c87751cf95
Tommy Palmer <hi@tommyp.org>
2021-01-03 15:29:13 +0000

    fix retro-bins.js formatting issues

--- src/retro-bins.js
+++ src/retro-bins.js
@@ -4,16 +4,16 @@ export default
       {
         "fileName": "Gloucester-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Gloucester"
+        "retroWhy": "branding",
+        "retroName": "Gloucester",
         "retroTwin": "Gloucester.jpg",
         "endDate": "2012",
       },
       {
         "fileName": "Macclesfield.jpg",
         "retro": "true",
-		"retroWhy": "abolished",
-        "retroName", "Macclesfield"
+        "retroWhy": "abolished",
+        "retroName": "Macclesfield",
         "retroTwin": "Cheshire-East.jpg",
         "startDate": "1974",
         "endDate": "2009",
@@ -23,8 +23,8 @@ export default
       {
         "fileName": "West-Wiltshire.jpg",
         "retro": "true",
-		"retroWhy": "abolished",
-        "retroName", "West Wiltshire"
+        "retroWhy": "abolished",
+        "retroName": "West Wiltshire",
         "retroTwin": "Wiltshire.jpg",
         "startDate": "1974",
         "endDate": "2009",
@@ -34,16 +34,16 @@ export default
       {
         "fileName": "North-Devon-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "North Devon"
+        "retroWhy": "branding",
+        "retroName": "North Devon",
         "retroTwin": "North-Devon.jpg",
         "endDate": "2009",
       },
       {
         "fileName": "Purbeck.jpg",
         "retro": "true",
-		"retroWhy": "abolished",
-        "retroName", "Purbeck"
+        "retroWhy": "abolished",
+        "retroName": "Purbeck",
         "retroTwin": "Dorset.jpg",
         "startDate": "1974",
         "endDate": "2019",
@@ -51,8 +51,8 @@ export default
       {
         "fileName": "Congleton.jpg",
         "retro": "true",
-		"retroWhy": "abolished",
-        "retroName", "Congleton"
+        "retroWhy": "abolished",
+        "retroName": "Congleton",
         "retroTwin": "Cheshire-East.jpg",
         "startDate": "1974",
         "endDate": "2009",
@@ -62,24 +62,24 @@ export default
       {
         "fileName": "Hart-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Hart"
+        "retroWhy": "branding",
+        "retroName": "Hart",
         "retroTwin": "Hart.jpg",
         "endDate": "2008",
       },
       {
         "fileName": "Southwark-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Southwark"
+        "retroWhy": "branding",
+        "retroName": "Southwark",
         "retroTwin": "Southwark.jpg",
         "endDate": "1999",
       },
       {
         "fileName": "Shepway.jpg",
         "retro": "true",
-		"retroWhy": "abolished",
-        "retroName", "Shepway"
+        "retroWhy": "abolished",
+        "retroName": "Shepway",
         "retroTwin": "Folkestone-and-Hythe.jpg",
         "startDate": "1974",
         "endDate": "2018",
@@ -87,56 +87,56 @@ export default
       {
         "fileName": "Chelmsford-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Chelmsford"
+        "retroWhy": "branding",
+        "retroName": "Chelmsford",
         "retroTwin": "Chelmsford.jpg",
         "endDate": "2012",
       },
       {
         "fileName": "Wyre-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Wyre"
+        "retroWhy": "branding",
+        "retroName": "Wyre",
         "retroTwin": "Wyre.jpg",
         "endDate": "1974",
       },
       {
         "fileName": "Bath-and-North-East-Somerset-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Bath and North East Somerset"
+        "retroWhy": "branding",
+        "retroName": "Bath and North East Somerset",
         "retroTwin": "Bath-and-North-East-Somerset.jpg",
         "endDate": "2008",
       },
       {
         "fileName": "Liverpool-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Liverpool"
+        "retroWhy": "branding",
+        "retroName": "Liverpool",
         "retroTwin": "Liverpool.jpg",
         "endDate": "2014",
       },
       {
         "fileName": "Reigate-and-Banstead.jpg-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Reigate and Banstead"
+        "retroWhy": "branding",
+        "retroName": "Reigate and Banstead",
         "retroTwin": "Reigate-and-Banstead.jpg",
         "endDate": "TBC",
       },
       {
         "fileName": "Lancaster-retro.jpg",
         "retro": "true",
-		"retroWhy": "branding",
-        "retroName", "Lancaster"
+        "retroWhy": "branding",
+        "retroName": "Lancaster",
         "retroTwin": "Lancashter.jpg",
         "endDate": "2014",
       },
       {
         "fileName": "Berwick-upon-Tweed.jpg",
         "retro": "true",
-		"retroWhy": "abolished",
-        "retroName", "Berwick-upon-Tweed"
+        "retroWhy": "abolished",
+        "retroName": "Berwick-upon-Tweed",
         "retroTwin": "Northumberland.jpg",
         "startDate": "1974",
         "endDate": "2009",
@@ -144,8 +144,8 @@ export default
       {
         "fileName": "Castle-Morpeth.jpg",
         "retro": "true",
-		"retroWhy": "abolished",
-        "retroName", "Castle Morpeth"
+        "retroWhy": "abolished",
+        "retroName": "Castle Morpeth",
         "retroTwin": "Northumberland.jpg",
         "startDate": "1974",
         "endDate": "2009",

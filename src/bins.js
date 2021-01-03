90888266f7c6bca895d8d98dc85a346f5330e02f
Tommy Palmer <hi@tommyp.org>
2021-01-03 15:48:14 +0000

    merge retro-bins into bins

--- src/bins.js
+++ src/bins.js
@@ -11,6 +11,155 @@ export default
         "colour": "211ƒ-blue"
       },
       {
+        "fileName": "Gloucester-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Gloucester",
+        "retroTwin": "Gloucester.jpg",
+        "endDate": "2012",
+      },
+      {
+        "fileName": "Macclesfield.jpg",
+        "retro": "true",
+        "retroWhy": "abolished",
+        "retroName": "Macclesfield",
+        "retroTwin": "Cheshire-East.jpg",
+        "startDate": "1974",
+        "endDate": "2009",
+        "contributorHandle": "@alicebartlett",
+        "contributorURL": "https://twitter.com/alicebartlett",
+      },
+      {
+        "fileName": "West-Wiltshire.jpg",
+        "retro": "true",
+        "retroWhy": "abolished",
+        "retroName": "West Wiltshire",
+        "retroTwin": "Wiltshire.jpg",
+        "startDate": "1974",
+        "endDate": "2009",
+        "contributorHandle": "@m.sergison",
+        "contributorURL": "https://www.instagram.com/m.sergison",
+      },
+      {
+        "fileName": "North-Devon-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "North Devon",
+        "retroTwin": "North-Devon.jpg",
+        "endDate": "2009",
+      },
+      {
+        "fileName": "Purbeck.jpg",
+        "retro": "true",
+        "retroWhy": "abolished",
+        "retroName": "Purbeck",
+        "retroTwin": "Dorset.jpg",
+        "startDate": "1974",
+        "endDate": "2019",
+      },
+      {
+        "fileName": "Congleton.jpg",
+        "retro": "true",
+        "retroWhy": "abolished",
+        "retroName": "Congleton",
+        "retroTwin": "Cheshire-East.jpg",
+        "startDate": "1974",
+        "endDate": "2009",
+        "contributorHandle": "@paulemorgan",
+        "contributorURL": "https://www.instagram.com/paulemorgan/",
+      },
+      {
+        "fileName": "Hart-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Hart",
+        "retroTwin": "Hart.jpg",
+        "endDate": "2008",
+      },
+      {
+        "fileName": "Southwark-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Southwark",
+        "retroTwin": "Southwark.jpg",
+        "endDate": "1999",
+      },
+      {
+        "fileName": "Shepway.jpg",
+        "retro": "true",
+        "retroWhy": "abolished",
+        "retroName": "Shepway",
+        "retroTwin": "Folkestone-and-Hythe.jpg",
+        "startDate": "1974",
+        "endDate": "2018",
+      },
+      {
+        "fileName": "Chelmsford-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Chelmsford",
+        "retroTwin": "Chelmsford.jpg",
+        "endDate": "2012",
+      },
+      {
+        "fileName": "Wyre-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Wyre",
+        "retroTwin": "Wyre.jpg",
+        "endDate": "1974",
+      },
+      {
+        "fileName": "Bath-and-North-East-Somerset-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Bath and North East Somerset",
+        "retroTwin": "Bath-and-North-East-Somerset.jpg",
+        "endDate": "2008",
+      },
+      {
+        "fileName": "Liverpool-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Liverpool",
+        "retroTwin": "Liverpool.jpg",
+        "endDate": "2014",
+      },
+      {
+        "fileName": "Reigate-and-Banstead.jpg-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Reigate and Banstead",
+        "retroTwin": "Reigate-and-Banstead.jpg",
+        "endDate": "TBC",
+      },
+      {
+        "fileName": "Lancaster-retro.jpg",
+        "retro": "true",
+        "retroWhy": "branding",
+        "retroName": "Lancaster",
+        "retroTwin": "Lancashter.jpg",
+        "endDate": "2014",
+      },
+      {
+        "fileName": "Berwick-upon-Tweed.jpg",
+        "retro": "true",
+        "retroWhy": "abolished",
+        "retroName": "Berwick-upon-Tweed",
+        "retroTwin": "Northumberland.jpg",
+        "startDate": "1974",
+        "endDate": "2009",
+      },
+      {
+        "fileName": "Castle-Morpeth.jpg",
+        "retro": "true",
+        "retroWhy": "abolished",
+        "retroName": "Castle Morpeth",
+        "retroTwin": "Northumberland.jpg",
+        "startDate": "1974",
+        "endDate": "2009",
+      },
+      {
         "fileName": "West-Lancashire.jpg",
         "localAuthorityCountry": "eng",
         "localAuthorityCode": "WLA",
@@ -35,7 +184,7 @@ export default
         "collectionDate": "6 December 2020",
         "contributorHandle": "Rachel Kirvan",
         "colour": "762-grayscale"
-      }, 
+      },
       {
         "fileName": "Kettering.jpg",
         "localAuthorityCountry": "eng",
@@ -44,7 +193,7 @@ export default
         "contributorHandle": "@toshenhoff",
         "contributorURL": "https://twitter.com/toshenhoff",
         "colour": "220-blue"
-      }, 
+      },
       {
         "fileName": "Halton.jpg",
         "localAuthorityCountry": "eng",
@@ -60,7 +209,7 @@ export default
         "localAuthorityCode": "COT",
         "collectionDate": "24 October 2020",
         "colour": "325-green"
-      }, 
+      },
       {
         "fileName": "Inverclyde.jpg",
         "localAuthorityCountry": "sct",
@@ -69,7 +218,7 @@ export default
         "contributorHandle": "@jamesmckechni_",
         "contributorURL": "https://twitter.com/jamesmckechni_",
         "colour": "600-brown"
-      }, 
+      },
       {
         "fileName": "High-Peak.jpg",
         "localAuthorityCountry": "eng",
@@ -78,7 +227,7 @@ export default
         "contributorHandle": "@ifranco_29",
         "contributorURL": "https://twitter.com/ifranco_29",
         "colour": "615-brown"
-      }, 
+      },
       {
         "fileName": "Highland.jpg",
         "localAuthorityCountry": "sct",
@@ -87,16 +236,16 @@ export default
         "contributorHandle": "@JoStraw1",
         "contributorURL": "https://twitter.com/JoStraw1",
         "colour": "325-green"
-      }, 
+      },
       {
         "fileName": "Gwynedd.jpg",
         "localAuthorityCountry": "wls",
         "localAuthorityCode": "GWN",
         "contributorHandle": "@keithemmerson",
-        "contributorURL": "https://twitter.com/keithemmerson",        
+        "contributorURL": "https://twitter.com/keithemmerson",
         "collectionDate": "1 October 2020",
         "colour": "600-brown"
-      },     
+      },
       {
         "fileName": "Derby.jpg",
         "localAuthorityCountry": "eng",
@@ -137,7 +286,7 @@ export default
         "contributorHandle": "@sirskelton",
         "contributorURL": "https://www.instagram.com/sirskelton",
         "colour": "310-green"
-      },  
+      },
       {
         "fileName": "North-Norfolk.jpg",
         "localAuthorityCountry": "eng",
@@ -162,14 +311,14 @@ export default
         "localAuthorityCode": "MTY",
         "collectionDate": "6 September 2020",
         "colour": "762-grayscale"
-      }, 
+      },
       {
         "fileName": "Neath-Port-Talbot.jpg",
         "localAuthorityCountry": "wls",
         "localAuthorityCode": "NTL",
         "collectionDate": "5 September 2020",
         "colour": "755-grayscale"
-      }, 
+      },
       {
         "fileName": "Argyll-and-Bute.jpg",
         "localAuthorityCountry": "sct",
@@ -177,7 +326,7 @@ export default
         "collectionDate": "4 September 2020",
         "contributorHandle": "Finlay Wilson",
         "colour": "291-blue"
-      },      
+      },
       {
         "fileName": "Aberdeen-City.jpg",
         "localAuthorityCountry": "sct",

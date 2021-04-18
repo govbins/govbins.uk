bf87dd321f780e1d64590bb21dca60b626c382ca
Tommy Palmer <hi@tommyp.org>
2021-04-18 11:20:32 +0100

    Clarify that the map is loading

--- pages/map.js
+++ pages/map.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import P from "../components/p";
 import H2 from "../components/h2";
 import Footer from "../components/footer";
-import data from "../src/bins"
+import data from "../src/bins";
 
 const Map = () => {
+  const [isLoaded, setLoaded] = useState(false);
+
   return (
     <>
       <div className="lg:w-2/3 lg:ml-40 px-5 pt-5 lg:px-0 text-2xl font-rubik">
         <H2>Map</H2>
         <P>{data.bins.filter((bin) => !bin.retro).length} bins catalogued</P>
-        <img src="/images/map.svg" />
+        <img onLoad={() => setLoaded(true)} src="/images/map.svg" />
+        {!isLoaded && "Map loading..."}
         <Footer />
       </div>
     </>
